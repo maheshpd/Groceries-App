5399cf59ad4498a528029f5c4b573635085384fb
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-01-02 21:49:42 +0530

    Initial Commit

---
+++ Groceries App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Groceries App
+//
+//  Created by Mahesh Prasad on 02/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Groceries App/Groceries_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Groceries_AppApp.swift
+//  Groceries App
+//
+//  Created by Mahesh Prasad on 02/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Groceries_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
